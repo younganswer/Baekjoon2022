f4827f1e39c324e3eccfa132a0d1fbde187c7e46
황영서 <hwang-youngseo@MacBookAir-of-YS.local>
2022-06-27 11:59:21 +0900

    Mon Jun 27, 2022 11:59

--- class_01/ex_2438/main.c
+++ class_01/ex_2438/main.c
@@ -3,8 +3,8 @@
 int main() {
     int N;
     scanf("%d", &N);
-    for (int i=1; i<=N; i++) {
-        for (int j=1; j<=i; j++) {
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<=i; j++) {
             printf("*");
         }
         printf("\n");
